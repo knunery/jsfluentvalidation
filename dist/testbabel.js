1a3944630564e68ec292e5517bbcdd8a04f0069e
Kyle Nunery <kyle.nunery@gmail.com>
2016-01-01 21:29:03 -0500

    test class construct with babeljs

--- dist/testbabel.js
+++ dist/testbabel.js
@@ -1,9 +1,19 @@
+"use strict";
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
 // Expression bodies
-var odds = evens.map(v => v + 1);
-var nums = evens.map((v, i) => v + i);
+var odds = evens.map(function (v) {
+  return v + 1;
+});
+var nums = evens.map(function (v, i) {
+  return v + i;
+});
 
 // Statement bodies
-nums.forEach(v => {
+nums.forEach(function (v) {
   if (v % 5 === 0) fives.push(v);
 });
 
@@ -11,7 +21,29 @@ nums.forEach(v => {
 var bob = {
   _name: "Bob",
   _friends: [],
-  printFriends() {
-    this._friends.forEach(f => console.log(this._name + " knows " + f));
+  printFriends: function printFriends() {
+    var _this = this;
+
+    this._friends.forEach(function (f) {
+      return console.log(_this._name + " knows " + f);
+    });
   }
 };
+
+var Test = (function () {
+  function Test() {
+    _classCallCheck(this, Test);
+  }
+
+  _createClass(Test, [{
+    key: "getItems",
+    value: function getItems() {
+      return [];
+    }
+  }, {
+    key: "saveItem",
+    value: function saveItem(item) {}
+  }]);
+
+  return Test;
+})();
--- src/testbabel.js
+++ src/testbabel.js
@@ -17,3 +17,15 @@ var bob = {
       console.log(this._name + " knows " + f));
   }
 };
+
+class Test {
+
+    getItems(){
+        return [];
+
+    }
+
+    saveItem(item){
+
+    }
+}
