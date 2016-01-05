d75e9a577bbe664a417eaf2b73aa0ce866bf2933
Kyle Nunery <kyle.nunery@gmail.com>
2016-01-04 22:19:03 -0500

    added failing test for a second property on the model

--- test/validateModel.js
+++ test/validateModel.js
@@ -25,5 +25,16 @@ describe('Customer Validation', function() {
       assert.equal(0, validationResult.errors.length);
     });
 
+    it('should fail when lastName is undefined', function() {
+      var customer = {firstName: "jessica"};
+
+     var validator = new customerValidator.CustomerValidator();
+
+      var validationResult = validator.validate(customer);
+
+      assert.equal(false, validationResult.isValid());
+      assert.equal(1, validationResult.errors.length);
+    });
+
   });
 });
