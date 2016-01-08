f5ca1fba0e5ed23b593056cdf17c90d109500136
Kyle Nunery <kyle.nunery@gmail.com>
2016-01-07 22:24:09 -0500

    wip email validator

--- dist/validator.js
+++ dist/validator.js
@@ -127,6 +127,7 @@ var CustomerValidator = (function (_AbstractValidator) {
 
 		_this.RuleFor("firstName").notNullOrUndefined();
 		_this.RuleFor("lastName").length(1, 10);
+		_this.RuleFor("email").emailAddress();
 		return _this;
 	}
 
@@ -175,6 +176,23 @@ var LengthValidator = (function () {
 	}]);
 
 	return LengthValidator;
+})();
+
+var EmailValidator = (function () {
+	function EmailValidator() {
+		_classCallCheck(this, EmailValidator);
+
+		this.expression = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-||_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+([a-z]+|\d|-|\.{0,1}|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])?([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/;
+	}
+
+	_createClass(EmailValidator, [{
+		key: 'isValid',
+		value: function isValid(context) {
+			return this.expression.test(context.propertyValue);
+		}
+	}]);
+
+	return EmailValidator;
 })();
 
 exports.CustomerValidator = CustomerValidator;
