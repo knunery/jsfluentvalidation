551dc964bb1f8df620d68f3c42fb33f7ddc5c9f2
Kyle Nunery <kyle.nunery@gmail.com>
2016-01-10 13:39:09 -0500

    wip emailValidator

--- dist/validator.js
+++ dist/validator.js
@@ -104,13 +104,17 @@ var RuleBuilder = (function () {
 	}, {
 		key: 'notNullOrUndefined',
 		value: function notNullOrUndefined() {
-
 			this.addValidator(new NotNullorEmptyValidator());
 		}
 	}, {
 		key: 'length',
 		value: function length(min, max) {
 			this.addValidator(new LengthValidator(min, max));
+		}
+	}, {
+		key: 'emailAddress',
+		value: function emailAddress() {
+			this.addValidator(new EmailValidator());
 		}
 	}]);
 
@@ -182,6 +186,9 @@ var EmailValidator = (function () {
 	function EmailValidator() {
 		_classCallCheck(this, EmailValidator);
 
+		this.message = function (propertyName) {
+			return "'${propertyName}' is not a valid email address.";
+		};
 		this.expression = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-||_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+([a-z]+|\d|-|\.{0,1}|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])?([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/;
 	}
 
@@ -229,4 +236,5 @@ var MoreThanValidator = (function () {
 	return MoreThanValidator;
 })();
 
+exports.EmailValidator = EmailValidator;
 exports.CustomerValidator = CustomerValidator;
---
+++ test/emailValidatorTests.js
@@ -0,0 +1,14 @@
+var assert = require('assert');
+var validator = require("../dist/validator"); 
+
+describe("EmailValidator", function() {
+	it("should be invalid email", function(){
+		var emailValidator = new validator.EmailValidator();
+
+		var context = {propertyName:"", propertyValue: "notAvalidEmailAddress"};
+
+		var isValid = emailValidator.isValid(context);
+
+		assert.equal(isValid, false);
+	});
+});
