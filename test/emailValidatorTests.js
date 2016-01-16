64cca76dcd5d950e9bf40097b8931bd7d60329af
Kyle Nunery <kyle.nunery@gmail.com>
2016-01-16 12:47:28 -0500

    added valid email test

--- test/emailValidatorTests.js
+++ test/emailValidatorTests.js
@@ -11,4 +11,14 @@ describe("EmailValidator", function() {
 
 		assert.equal(isValid, false);
 	});
+
+	it("should be VALID email", function (){
+		var emailValidator = new validator.EmailValidator();
+
+		var context = {propertyName:"", propertyValue: "test@test.net"};
+
+		var isValid = emailValidator.isValid(context);
+
+		assert.equal(isValid, true);
+	});
 });
