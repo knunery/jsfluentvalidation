3a5f9057d1bfd3d09d2b1af5d70e66b6e50177d6
Kyle Nunery <kyle.nunery@gmail.com>
2016-01-20 13:57:40 -0500

    added watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 var gulp = require("gulp");
 var babel = require("gulp-babel");
 
-gulp.task("default", function () {
+gulp.task('watch', function() {
+	gulp.watch(['src/**/*.js'],['scripts']);
+});
+
+gulp.task("scripts", function () {
   return gulp.src("src/**/*.js")
     .pipe(babel())
     .pipe(gulp.dest("dist"));
 });
+
+gulp.task('default', ['scripts', 'watch']);
